4c57b69c3768281d73e1f12a9aa7cbf6d0dd7651
Diana Canela <DCanela80@gmail.com>
2023-11-29 14:17:01 -0700

    exercises completed

--- app.js
+++ app.js
@@ -2,3 +2,29 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const searchButton = document.getElementById("submitSearch");
+const searchInput = document.getElementById("searchWord");
+const imageElement = document.querySelector("#imageContainer > img");
+const feedbackPara = document.querySelector("#feedback");
+const API_KEY = "6HGcTaY0TZmT91XxhrCIwPYwHTpVTZlT";
+
+//Exercise 2-4
+searchButton.addEventListener("click", () => {
+    console.dir(searchInput.value);
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchInput.value}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        imageElement.src = data.data.images.original.url;
+        searchInput.value = "";
+        feedbackPara.textContent = "";
+    })
+    .catch((err) => {
+        console.error(err);
+        console.log(err.message);
+        feedbackPara.textContent = err.message;
+    })
+});
+
